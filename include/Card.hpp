2ffc9c884c5f5e829be88b354a288cbe0f8ec30e
Omar <zlac353@live.rhul.ac.uk>
2024-05-27 20:17:26 +0100

    Update `Card` and broken it into .hpp and .cpp

--- include/Card.hpp
+++ include/Card.hpp
@@ -9,59 +9,44 @@
 
 class Card {
 public:
-    std::string name;
-    std::string cardType;
-    std::string description;
-    std::vector<Effect*> effects{};
-    bool flipped = false;
-
-    Card(std::string n, std::string ct, std::string d) : name(n), cardType(ct), description(d) {}
+    Card(std::string n, std::string ct, std::string d);
+    ~Card();
 
     virtual void activate(Player* player, Player* opponent) = 0; // Pure virtual method
+    virtual void activateEffects(Player* player, Player* opponent);
 
-    virtual void activateEffects(Player* player, Player* opponent) {
-        for (Effect* effect : effects){
-            effect->activate(player, opponent);
-        }
-    }
-
+public:
+    const std::string name;
+    const std::string cardType;
+    const std::string description;
+    const std::vector<Effect*> effects{};
+    bool flipped = false;
 };
 
 class MonsterCard: public Card {
 public:
+    MonsterCard(std::string n, std::string d, int atk, int def, int lvl);
+
+    bool isDestroyedByBattle(int atk_points) const;
+
+public:
     int attack;
     int defense;
     int level;
-
-    MonsterCard(std::string n, std::string d, int atk, int def, int lvl)
-        : Card(n, "Monster", d), attack(atk), defense(def), level(lvl){}
-
-    bool isDestroyedByBattle(int atk_points) const {
-        return atk_points >=defense;
-    }
-
-
-
 };
 
 class SpellCard: public Card {
 public:
-    SpellCard(std::string n, std::string  d) : Card(n, "Spell", d){}
+    SpellCard(std::string n, std::string d);
 
-    void activate(Player* player, Player* opponent) override {
-        std::cout << "Activating spell: " << name << std::endl;
-        activateEffects(player, opponent);
-    }
+    void activate(Player* player, Player* opponent) override;
 };
 
 class TrapCard : public Card {
 public:
-    TrapCard(std::string n, std::string d) : Card(n, "Trap", d) {}
+    TrapCard(std::string n, std::string d);
 
-    void activate(Player* player, Player* opponent) override {
-        std::cout << "Activating trap: " << name << "!\n";
-        activateEffects(player, opponent);
-    }
+    void activate(Player* player, Player* opponent) override;
 };
 
 
--- src/Card.cpp
+++ src/Card.cpp
@@ -1,1 +1,39 @@
 #include "../include/Card.hpp"
+
+Card::Card(std::string n, std::string ct, std::string d)
+    : name(n), cardType(ct), description(d) {}
+
+Card::~Card() {
+    for (Effect* effect: effects) {
+        effect = nullptr;
+    }
+}
+
+void Card::activateEffects(Player* player, Player* opponent) {
+    for (Effect* effect: effects){
+        effect->activate(player, opponent);
+    }
+}
+
+
+MonsterCard::MonsterCard(std::string n, std::string d, int atk, int def, int lvl)
+        : Card(n, "Monster", d), attack(atk), defense(def), level(lvl){}
+
+bool MonsterCard::isDestroyedByBattle(int atk_points) const {
+    return atk_points >=defense;
+}
+
+SpellCard::SpellCard(std::string n, std::string d) : Card(n, "Spell", d){}
+
+void SpellCard::activate(Player *player, Player *opponent) {
+    std::cout << "Activating spell: " << name << std::endl;
+    activateEffects(player, opponent);
+}
+
+TrapCard::TrapCard(std::string n, std::string d)
+    : Card(n, "Trap", d) {}
+
+void TrapCard::activate(Player *player, Player *opponent) {
+    std::cout << "Activating trap: " << name << "!\n";
+    activateEffects(player, opponent);
+}
